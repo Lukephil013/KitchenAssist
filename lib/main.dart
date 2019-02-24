6f0d29e64b74c491e273e2245ef510a983c9210e
Luke Philips <lukephil013@gmail.com>
2019-02-24 09:46:55 -0800

    Able to build list of in-house items

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:kitchen_assist/appScreens/titleScreen.dart';
 
-void main(){
-  runApp(
-      MaterialApp(
-        title: "Kitchen Assist",
-        home: FoodList(),
-    )
-  );
+void main() => runApp(new KitchenAssist());
+
+
+class KitchenAssist extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Kitchen Assist',
+      home: new FoodList(),
+    );
+  }
 }
 
 class FoodList extends StatefulWidget{
   @override
-    FoodListState createState() => FoodListState();
+  createState() => new FoodListState();
+}
+
+class FoodListState extends State<FoodList> {
+  List<String> _foodItems = [];
+
+  void _addFoodItem(String item) {
+    if(item.length > 0){
+      setState(() => _foodItems.add(item));
+    }
   }
 
-class FoodListState extends State<FoodList>{
-  int value = 2;
-  _addItem() {
-    setState(() {
-      value = value + 1;
-    });
+  Widget _buildFoodList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _foodItems.length) {
+          return _buildFoodItem(_foodItems[index]);
+        }
+      },
+    );
+  }
+
+  Widget _buildFoodItem(String item) {
+    return new ListTile(
+        title: new Text(item)
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("MyApp"),
+      appBar: new AppBar(
+          title: new Text('In-House Food')
       ),
-      body: ListView.builder(
-          itemCount: this.value,
-          itemBuilder: (context, index) => this._buildRow(index)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        child: Icon(Icons.add),
+      body: _buildFoodList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _pushItemFoodScreen,
+          tooltip: 'Add Item',
+          child: new Icon(Icons.add)
       ),
     );
   }
 
-
-  _buildRow(int index){
-    return Text("Item " + index.toString());
+  void _pushItemFoodScreen() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                      title: new Text('Add a new item')
+                  ),
+                  body: new TextField(
+                    autofocus: true,
+                    onSubmitted: (val) {
+                      _addFoodItem(val);
+                      Navigator.pop(context);
+                    },
+                    decoration: new InputDecoration(
+                        hintText: 'Enter a food item...',
+                        contentPadding: const EdgeInsets.all(16.0)
+                    ),
+                  )
+              );
+            }
+        )
+    );
   }
 }
 
-//Widget getListView(){
-//  var listView = ListView(
-//    children: <Widget>[
-//      ListTile(
-//        leading: Icon(Icons.landscape),
-//        title: Text("Landscape"),
-//        subtitle: Text("Beautiful View!"),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ],
-//  );
-//  return listView;
-//}
-//
-//Widget foodInputTxtField = TextField(
-//  decoration: InputDecoration(
-//    border: InputBorder.none,
-//    hintText: 'Enter food item.'
-//  ),
-//);
-//
 
-
-//class FoodListState extends State<FoodList>{
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("In-Home Food Items"),
-//      ),
-//      body: getListView(),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: (){
-//        },
-//        child: Icon(Icons.fastfood),
-//        backgroundColor: Colors.lightGreen,
-//        foregroundColor: Colors.black,
-//        tooltip: 'Add Item to Food List',
-//      ),
-//    );
-//  }
-//}
