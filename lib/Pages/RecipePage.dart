1441261d761c136ca4c6b6a2ce9fe2eaf538714d
Luke Philips <lukephil013@gmail.com>
2019-03-11 18:10:38 -0700

    Random API implemented.

--- lib/Pages/RecipePage.dart
+++ lib/Pages/RecipePage.dart
@@ -1,11 +1,70 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
-class recipePage extends StatelessWidget{
+class recipePage extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      color: Colors.blue,
-    );
+  createState() => new recipePageState();
+}
+
+class recipePageState extends State<recipePage>{
+  final String url = "https://swapi.co/api/starships/";
+  List data;
+
+  Future<String> getSWData() async{
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var resBody = json.decode(res.body);
+      data =  resBody["results"];
+    });
+    return "Success!";
+  }
+
+  @override
+
+  Widget build(BuildContext context) => Scaffold(
+    body: ListView.builder(
+      itemCount: data == null ? 0 : data.length,
+      itemBuilder: (BuildContext context, int index){
+        return new Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Card(
+                  child: Container(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text(data[index]["name"],
+                    style:
+                      TextStyle(fontSize: 18.0,
+                      color: Colors.black54)
+                  )),
+                ),
+                Card(
+                  child: Container(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text(data[index]["model"],
+                    style:
+                      TextStyle(fontSize: 18.0,
+                    color: Colors.black54
+                      )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
+
+  @override
+  void initState(){
+    super.initState();
+    this.getSWData();
   }
 }
+
